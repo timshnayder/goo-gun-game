34ddc440aa2a7ae7c62f2b2e080f6c5fb93826e1
TheCutestBoy <addrloyola@gmail.com>
2025-04-15 10:59:07 -0400

    tithoy

--- scenes/levels/main.gd
+++ scenes/levels/main.gd
@@ -3,7 +3,7 @@ extends Node2D
 @onready var pause_menu = $Player/Camera2D/CanvasLayer/PauseMenu
 var paused = false
 
-func _process(delta):
+func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("pause"):
 		pauseMenu()
 		
