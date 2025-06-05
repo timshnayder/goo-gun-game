408f2bcec20e1083b32a151fba95b270cf11cf21
timshnayder <timshnayder@gmail.com>
2025-06-05 12:16:45 -0400

    hopefully this fixes everything

--- scenes/main_menu.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-
-func _on_quit_button_pressed() -> void:
-	get_tree().quit()
-
-
-func _on_start_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/BuildSet/level_1_set.tscn")
